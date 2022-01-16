6698cf47bd4338dad8f8fcae43d1b403894383a1
kenoze <94803772+kenoze@users.noreply.github.com>
2022-01-16 21:05:18 +0100

    Add files via upload

---
+++ fskAdresar.js
@@ -0,0 +1,53 @@
+function getStudentsFromStorage() {
+    return JSON.parse(sessionStorage.getItem('studenti'))
+}
+
+function setStudentsToStorage(students) {
+    sessionStorage.setItem('studenti', JSON.stringify(students))
+}
+
+function ubaciStudenta(a,b,c,d,e,f){
+    var studenti = []
+    var studentiIzStorage = JSON.parse(sessionStorage.getItem('studenti'))
+    if (studentiIzStorage != null) studenti = studentiIzStorage
+    let student = {
+        ime: a,
+        prezime: b,
+        index: c,
+        smjer: d,
+        ciklus: e,
+        semestar: f
+    }
+    studenti.push(student)
+    setStudentsToStorage(studenti)
+    document.getElementById('ime').value = "";
+    document.getElementById('prezime').value = "";
+    document.getElementById('index').value = "";
+    document.getElementById('smjer').value = "";
+    document.getElementById('ciklus').value = "";
+    document.getElementById('semestar').value = "";
+}
+
+function prikaziStudentaStorage(StudentID){
+    console.log(getStudentsFromStorage());
+    var trazeniStudent = pronadjiStudenta(StudentID);
+    if (trazeniStudent === undefined){
+        document.getElementById("ispis").innerHTML = 'Traženi student nije u sistemu';
+    } else {
+        document.getElementById("ispis").innerHTML = 'Traženi student: ' + trazeniStudent.ime + ' ' + trazeniStudent.prezime;
+    }
+}
+
+function pronadjiStudenta(ID){
+    return getStudentsFromStorage().find(student => student.index === ID);
+}
+
+function lista(){
+var studenti = getStudentsFromStorage();
+var listaStudenata = "";
+for (let i = 0; i < studenti.length; i++) {
+    listaStudenata += `<br><br>${studenti[i].index}
+    ${studenti[i].ime} ${studenti[i].prezime}<br>`
+ }
+document.getElementById("ispis").innerHTML = "Lista studenata je " + listaStudenata;
+}
